60002d79b25d9bd20a43a19539883c0d6ae4f407
leolima13 <limarodriguesleonardo@gmail.com>
2022-02-23 01:24:19 -0300

    Update test_validacaodesequenciadecaracteres.py

--- Problema 2/test_validacaodesequenciadecaracteres.py
+++ Problema 2/test_validacaodesequenciadecaracteres.py
@@ -6,7 +6,7 @@ Obj = ValidacaoDeSequenciaDeCaracteres()
 
 @pytest.mark.parametrize(
     'input_string_valida',
-    ['{[()]}', '{[()]}', '([])', '{{[[(())]]}}', '{[()([])]}', '{{[[(())]]}}']
+    ['{[()]}', '{[()]}', '([])', '{{[[(())]]}}', '{[()([])]}', '{{[[(())]]}}', '']
 )
 def test_check_brackets_valido(input_string_valida):
     # Testa os casos onde todos os pares de caractres são válidos
